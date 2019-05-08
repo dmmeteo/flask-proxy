0ddcfed66e2d2ba500d21ba5dfefba1e92761730
Dmitry Sichkar <dmmeteo@gmail.com>
2019-05-08 14:24:48 +0300

    :zap: Replace functions
    Add replace feature

--- app.py
+++ app.py
@@ -3,13 +3,30 @@ import requests
 from flask import Flask
 from bs4 import BeautifulSoup as bs
 
+PATTERN_URL = 'https://habr.com/'
+PATTERN_WORD = r'\b[\w]{6}\b'
 app = Flask(__name__)
+
+
+def replace_words(html):
+    for word in html.find_all(text=re.compile(PATTERN_WORD)):
+        word.replace_with(re.sub(PATTERN_WORD, lambda w: w.group(0)+'™', word))
+    return html
+
+
+def replace_hrefs(html):
+    for a in html.find_all('a', 'href' is not None):
+        a['href'] = a['href'].replace(PATTERN_URL, 'http://127.0.0.1:5000/')
+    return html
 
 
 @app.route('/')
 @app.route('/<path:path>')
 def index(path=''):
-    r = requests.get('habr.com/' + path)
+    r = requests.get(PATTERN_URL + path)
+    res = bs(r.text, 'lxml')
+    res = replace_hrefs(res)
+    res = replace_words(res)
     return res.prettify('utf-8')
 
 
